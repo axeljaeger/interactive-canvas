b5151b17f1c59b549a8d4c225b8a47a6bf3624e0
Axel Jäger <axeljaeger@gmail.com>
2024-11-12 13:56:39 +0100

    Add ES2022 also in vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
     base: "https://axeljaeger.github.io/interactive-canvas/",
+    build: {
+      target: "ES2022"
+    },
   })
 );
